02953bf74006d1c71f78521912ec6864e9511589
KaiwenJon <kaiwenjon23@gmail.com>
2023-03-12 23:58:46 -0500

    Collision

--- MP2/mp2.js
+++ MP2/mp2.js
@@ -12,7 +12,7 @@
 
 // Required: 3D Illini Logo self-rotating and camera zoomin
 function draw0(milliseconds) {
-    seconds = milliseconds/1000
+    let seconds = milliseconds/1000
     // window.m = IdentityMatrix
     // camera_pos = [0, 0, 1.5, 1]
     // camera_pos = m4mul(m4trans(0, 0, Math.sin(seconds)+1), camera_pos)
@@ -45,7 +45,7 @@ function draw0(milliseconds) {
 
 // Required: 2D Illini Logo moving, rotating, and scaling
 function draw1(milliseconds) {
-    seconds = milliseconds/1000
+    let seconds = milliseconds/1000
     window.m = m4mul(m4rotZ(seconds*1.5), m4scale(Math.sin(seconds*2)/2+1, Math.sin(seconds*2)/2+1, 1))
     window.m = m4mul(m4trans(Math.sin(seconds)/2, 0, 0), window.m)
     window.v = IdentityMatrix
@@ -64,7 +64,7 @@ function draw1(milliseconds) {
 
 // Optional 1: CPU-based vertex movement
 function draw2(milliseconds) {
-    seconds = milliseconds/1000
+    let seconds = milliseconds/1000
     // window.m = m4rotY(seconds)
     // camera_pos = [0, 0, 1.5, 1]
     // camera_pos = m4mul(m4trans(1, 1, 0), camera_pos)
@@ -81,22 +81,23 @@ function draw2(milliseconds) {
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.useProgram(program)
 
-    vsIn = "position"
-    data = window.models[2].attributes[vsIn]
-    mode = gl.DYNAMIC_DRAW
-    newData = []
-    offset = (Math.sin(seconds)*0.5+0.5)
-    scale = 0.5
+    let vsIn = "position"
+    let data = window.models[2].attributes[vsIn]
+    let mode = gl.DYNAMIC_DRAW
+    let newData = []
+    let offset = (Math.sin(seconds)*0.5+0.5)
+    let scale = 0.5
     data.forEach((item, index)=>{
         if(index < 6){
             item = m4mul(m4rotZ(seconds), [...item, 1]).slice(0, 3) 
             newItem = [(item[0]+offset)*scale, (item[1]+offset)*scale, item[2]*scale]
+            newData.push(newItem)
         }
         else if(index < 12){
             item = m4mul(m4rotZ(-seconds), [...item, 1]).slice(0, 3) 
             newItem = [(item[0]-offset)*scale, (item[1]-offset)*scale, item[2]*scale]
+            newData.push(newItem)
         }
-        newData.push(newItem)
     })
     supplyDataBuffer(newData, program, vsIn, mode)
 
@@ -111,15 +112,15 @@ function draw2(milliseconds) {
 
 // Optional 2: GPU_based vertex movement
 function draw3(milliseconds) {
-    seconds = milliseconds/1000
+    let seconds = milliseconds/1000
     window.m = IdentityMatrix
     window.v = IdentityMatrix
     window.p = IdentityMatrix
 
-    selfRotate = m4rotZ(seconds)
-    d = (Math.sin(seconds)*0.5+0.5)
-    offset1 = m4trans(0.2, 0.2, 0)
-    offset2 = m4trans(-0.2, -0.2, 0)
+    let selfRotate = m4rotZ(seconds)
+    let d = (Math.sin(seconds)*0.5+0.5)
+    let offset1 = m4trans(0.2, 0.2, 0)
+    let offset2 = m4trans(-0.2, -0.2, 0)
 
     gl.clearColor(...IlliniBlue)
     gl.clear(gl.COLOR_BUFFER_BIT)
@@ -137,6 +138,215 @@ function draw3(milliseconds) {
     window.pending = requestAnimationFrame(draw3)
 }
 
+// Optional 3: Collision
+function draw4(milliseconds) {
+    let offset = m4trans(logo1velocity[0]*0.01, logo1velocity[1]*0.01, 0)
+    window.m = IdentityMatrix
+    window.v = IdentityMatrix
+    window.p = IdentityMatrix
+    gl.clearColor(...IlliniBlue)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    gl.useProgram(program)
+
+
+    let vsIn = "position"
+    let data = window.logo1data
+    let mode = gl.DYNAMIC_DRAW
+    let newData = []
+
+    let bordersX = []
+    let bordersY = []
+    bordersX.push([1, 0, -1]) // x =  1
+    bordersX.push([1, 0,  1]) // x = -1
+    bordersY.push([0, 1, -1]) // y =  1
+    bordersY.push([0, 1,  1]) // y = -1
+    let hitBorderX = false
+    let hitBorderY = false
+    let hitOtherX = false
+    let hitOtherY = false
+    data.forEach((item, index)=>{
+        let newPosition = m4mul(offset, [...item, 1]).slice(0, 3)
+        let point2D = [newPosition[0], newPosition[1], 1]
+        if(checkLine(point2D, bordersX[0]) * checkLine(point2D, bordersX[1]) > 0){
+            // console.log("Hit!")
+            hitBorderX = true
+        }
+        if(checkLine(point2D, bordersY[0]) * checkLine(point2D, bordersY[1]) > 0){
+            // console.log("Hit!")
+            hitBorderY = true
+        }
+
+
+        if(checkLine(point2D, logo2BordersX[0]) * checkLine(point2D, logo2BordersX[1]) < 0){
+            // console.log("Hit!OtherX")
+            hitOtherX = true
+        }
+        if(checkLine(point2D, logo2BordersY[0]) * checkLine(point2D, logo2BordersY[1]) < 0){
+            // console.log("Hit!OtherY")
+            hitOtherY = true
+        }
+        newData.push(newPosition)
+    })
+    
+    if(hitBorderX){
+        window.logo1velocity = [-logo1velocity[0], logo1velocity[1], 0]
+        logo1Count = 0
+        newData.forEach((item, i)=>{
+            offset = m4trans(logo1velocity[0]*0.01, logo1velocity[1]*0.01, 0)
+            return (m4mul(offset, [...item, 1]).slice(0, 3))
+        })
+        // console.log('CHange direction!')
+    }
+    if(hitBorderY){
+        window.logo1velocity = [logo1velocity[0], -logo1velocity[1], 0]
+        logo1Count = 0
+        newData.forEach((item, i)=>{
+            offset = m4trans(logo1velocity[0]*0.01, logo1velocity[1]*0.01, 0)
+            return (m4mul(offset, [...item, 1]).slice(0, 3))
+        })
+    }
+    if(hitOtherX && hitOtherY){
+        window.logo1velocity = [-logo1velocity[0], -logo1velocity[1], 0]
+        window.logo2velocity = [-logo2velocity[0], -logo2velocity[1], 0]
+    }
+    
+    supplyDataBuffer(newData, program, vsIn, mode)
+    window.logo1data = newData
+
+
+    gl.bindVertexArray(geom.vao)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'mv'), false, IdentityMatrix)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'p'), false, IdentityMatrix)
+    gl.drawElements(geom.mode, geom.count, geom.type, 0)
+
+    //======================= Logo 2 =========================
+    offset = m4trans(logo2velocity[0]*0.01, logo2velocity[1]*0.01, 0)
+    data = window.logo2data
+
+    hitBorderX = false
+    hitBorderY = false
+    newData = []
+    let topLeft
+    let bottomRight
+    data.forEach((item, index)=>{
+        let newPosition = m4mul(offset, [...item, 1]).slice(0, 3)
+        let point2D = [newPosition[0], newPosition[1], 1]
+        if(checkLine(point2D, bordersX[0]) * checkLine(point2D, bordersX[1]) > 0){
+            // console.log("Hit!")
+            hitBorderX = true
+        }
+        if(checkLine(point2D, bordersY[0]) * checkLine(point2D, bordersY[1]) > 0){
+            // console.log("Hit!")
+            hitBorderY = true
+        }
+        newData.push(newPosition)
+
+
+        // update borders
+        // top left 0, top right 1, bottom left 10, bottom right 11
+        if(index == 0){
+            topLeft = [newPosition[0], newPosition[1]]
+        }
+        else if(index == 11){
+            bottomRight = [newPosition[0], newPosition[1]]
+        }
+    })
+    // console.log("TOPLEFT", topLeft)
+    // console.log("BOTTOMRIGHT", bottomRight)
+    logo2BordersX = [[1, 0, -topLeft[0]], [1, 0, -bottomRight[0]]]
+    logo2BordersY = [[0, 1, -topLeft[1]], [0, 1, -bottomRight[1]]]
+    
+    if(hitBorderX){
+        window.logo2velocity = [-logo2velocity[0], logo2velocity[1], 0]
+        newData.forEach((item, i)=>{
+            offset = m4trans(logo2velocity[0]*0.01, logo2velocity[1]*0.01, 0)
+            return (m4mul(offset, [...item, 1]).slice(0, 3))
+        })
+        // console.log('CHange direction!')
+    }
+    if(hitBorderY){
+        window.logo2velocity = [logo2velocity[0], -logo2velocity[1], 0]
+        newData.forEach((item, i)=>{
+            offset = m4trans(logo2velocity[0]*0.01, logo2velocity[1]*0.01, 0)
+            return (m4mul(offset, [...item, 1]).slice(0, 3))
+        })
+    }
+
+
+    
+    supplyDataBuffer(newData, program, vsIn, mode)
+    window.logo2data = newData
+
+
+    gl.bindVertexArray(geom.vao)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'mv'), false, IdentityMatrix)
+    gl.uniformMatrix4fv(gl.getUniformLocation(program, 'p'), false, IdentityMatrix)
+    gl.drawElements(geom.mode, geom.count, geom.type, 0)
+
+
+    window.pending = requestAnimationFrame(draw4)
+}
+
+function checkLine(point, line){
+    // line: a, b, c (ax+by+c=0), always let a > 0,
+    // then sign(ax0 + by0 + c) > 0 tells you that point is at right
+    // point (x0, y0, 1)
+    // return sign(ax0 + by0 + c)
+
+    // now way a and b are both 0
+    if(line[0] == 0){
+        // by + c = 0
+        if(line[1] < 0){
+            // b < 0
+            return dot(line, point) < 0 ? -1 : 1
+        }
+        else if(line[1] > 0){
+            // b > 0
+            return dot(line, point) > 0 ? 1 : -1
+        }
+        else{
+            console.log("No way.")
+        }
+    }
+    else if (line[1] == 0){
+        // ax + c = 0
+        if(line[0] < 0){
+            // a < 0
+            return dot(line, point) < 0 ? -1 : 1
+        }
+        else if(line[0] > 0){
+            // a > 0
+            return dot(line, point) > 0 ? 1 : -1
+        }
+        else{
+            console.log("No way.")
+        }
+    }
+}
+
+function initCollision(){
+    scale1 = 0.8
+    scale2 = 0.4
+    window.logo1data = window.models[4].attributes['position']
+    newData = []
+    logo1data.forEach((item, index)=>{
+        let newPosition = m4mul(m4trans(0.5, 0, 0), m4mul(m4scale(scale1, scale1, 1), [...item, 1])).slice(0, 3)
+        newData.push(newPosition)
+    })
+    logo1data = newData
+    window.logo1velocity = [1.2, 0.8, 0]
+
+    window.logo2data = window.models[4].attributes['position']
+    newData = []
+    logo2data.forEach((item, index)=>{
+        let newPosition = m4mul(m4trans(-0.5, 0, 0), m4mul(m4scale(scale2, scale2, 1), [...item, 1])).slice(0, 3)
+        newData.push(newPosition)
+    })
+    logo2data = newData
+    window.logo2velocity = [-0.7, -0.8, 0]
+    window.logo2BordersX = [[1, 0, 0.15], [1, 0, -0.15]]
+    window.logo2BordersY = [[0, 1, 0.25], [0, 1, -0.25]]
+}
 
 /** Callback for when the radio button selection changes */
 function radioChanged() {
@@ -148,7 +358,9 @@ function radioChanged() {
     window.program = compileAndLinkGLSL(vs, fs)
     const model = window.models[window.nowChosen]
     window.geom = setupGeomery(model)
-
+    if(nowChosen == 4){
+        initCollision()
+    }
     window.pending = requestAnimationFrame(window['draw'+nowChosen])
 }
 
